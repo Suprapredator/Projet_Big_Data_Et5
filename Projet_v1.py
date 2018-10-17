2a41201570029d3049f0a753b16249e95fbf6ad2
supra <quentin.jardet@u-psud.fr>
2018-10-17 17:37:38 +0200

    First commit

--- Projet_v1.py
+++ Projet_v1.py
@@ -7,4 +7,14 @@ Created on Wed Oct 10 13:37:53 2018
 
 import numpy as np;
 import matplotlib.pyplot as plt;
-from scipy.io import loadmat;    
+from scipy.io import loadmat;
+
+if __name__ == "__main__":
+
+    train_data = loadmat('../train_32x32.mat')
+    test_data = loadmat('../test_32x32.mat')
+
+    image_idx = 0
+    print('Label:', train_data['y'][image_idx])
+    plt.imshow(train_data['X'][:, :, :, image_idx])
+    plt.show()
